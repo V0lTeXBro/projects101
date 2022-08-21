65cb224c1f32ad5bfc47018ec16967b06914a3f3
tmeerkheir <tmer.kh99@gmail.com>
2022-08-21 18:37:40 +0300

    queryselectors examples

--- stop-watch/stop-watch.js
+++ stop-watch/stop-watch.js
@@ -1,1 +1,27 @@
 console.log('stop watch');
+
+
+
+const stopWatchView = document.querySelector('#stop-watch-view');
+console.log('id', stopWatchView);
+
+const pTag = document.querySelector('p');
+console.log('pTag', pTag);
+
+
+const myClass = document.querySelector('.my-class');
+console.log('myclass', myClass);
+
+const allListItems = document.querySelectorAll('.list-item');
+console.log(allListItems);
+let counter = 1;
+allListItems.forEach(function (listItem) {
+    console.log('list item', listItem);
+    listItem.innerHTML = counter;
+    counter++;
+});
+
+const arr = [1, 2, 3, 4, 5];
+arr.forEach(function (value) {
+    console.log(value);
+});
