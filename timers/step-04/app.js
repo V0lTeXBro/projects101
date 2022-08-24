8ccbae3edda8062b2f2b9638e3f213b668f42e13
tmeerkheir <tmer.kh99@gmail.com>
2022-08-24 19:59:40 +0300

    make button and input and connect them together

--- timers/step-04/app.js
+++ timers/step-04/app.js
@@ -1,7 +1,18 @@
 import Timer from './timers.js';
 
-const t1 = new Timer(50);
-console.log('timer 1', t1);
+//const t1 = new Timer(50);
+//console.log('timer 1', t1);
 
-const t2 = new Timer(40);
-console.log('timer 2', t2);
+//const t2 = new Timer(40);
+//console.log('timer 2', t2);
+
+document.querySelector('#create-timer').addEventListener('click', addTimer);
+
+const timerinput = document.querySelector('#time-input');
+
+function addTimer() {
+    console.log('add new timer');
+    const initialTime = timerinput.value;
+    const timer = new Timer(initialTime);
+    console.log('timer', timer);
+}
